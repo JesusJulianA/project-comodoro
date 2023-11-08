7b49dc98e1460149ab16f04cbaa81728060f8b40
jesusabreug <127265825+jesusabreug@users.noreply.github.com>
2023-11-07 22:03:13 -0400

    feat(molecules): update components in src/components/molecules/

---
+++ src/components/molecules/header-styles/index.jsx
@@ -0,0 +1,14 @@
+// /src/components/molecules/styles/headerStyles.js
+import { StyleSheet } from "react-native";
+
+export const headerStyles = StyleSheet.create({
+  itemStyle: {
+    alignItems: "center",
+    width: "33%",
+    borderWidth: 3,
+    padding: 5,
+    borderRadius: 10,
+    borderColor: "white",
+    marginVertical: 20,
+  }
+});
---
+++ src/components/molecules/header/index.jsx
@@ -0,0 +1,31 @@
+// /src/components/molecules/Header.js
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { headerStyles } from "../header-styles/index.jsx";
+
+const options = ["Pomodoro", "Short Break", "Long Break"];
+
+export default function Header({ currentTime, setCurrentTime, setTime }) {
+  function handlePress(index) {
+    const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+    setCurrentTime(index);
+    setTime(newTime * 60);
+  }
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {options.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => handlePress(index)}
+          style={[
+            headerStyles.itemStyle,
+            currentTime !== index && { borderColor: "transparent" },
+          ]}
+        >
+          <Text style={{ fontWeight: "bold" }}>{item}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
